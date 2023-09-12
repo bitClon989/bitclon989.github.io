7ca7a8f03c8858df20562ba5ecbe1294b64d43a9
krunal <krunal@krunals-MacBook-Pro.local>
2023-09-12 16:48:48 +0530

    logout button fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "317ed41d63d32746ea56c2d1065a6f57",
-"index.html": "9b64b33e0a7d4ccdd8dd272b66333a6a",
-"/": "9b64b33e0a7d4ccdd8dd272b66333a6a",
-"main.dart.js": "d6f6a13a0317175348aaf0dfcadf9772",
+"index.html": "7f802d5c4c09fc00de9af9056b810301",
+"/": "7f802d5c4c09fc00de9af9056b810301",
+"main.dart.js": "d6869d5e39dbb8bcac3c924ffc0cd451",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "3f04c2104d54639b90ad24a823f3ea10",
 "icons/Icon-192.png": "51ca05531b67619a3cea4191d886aa01",
